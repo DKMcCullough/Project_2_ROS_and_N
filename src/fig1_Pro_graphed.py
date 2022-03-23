661f6169b96b8f615b3d56369f89f6c02a4056ec
Donna 'Katie' McCullough <dmccull8@vols.utk.edu>
2022-03-23 17:29:30 -0400

    restructure

---
+++ src/fig1_Pro_graphed.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 23 17:13:23 2022
+
+
+
+name:   All_modeled.py 
+
+location: /Users/dkm/Documents/Talmy_research/Zinser_and_Ben/Project_1_nutrient_additions/src
+
+@author: dkm
+
+
+goal: import and all treatments of HOOH on Pro iin unammended sea water 
+
+
+working on: importing and visualising data 
+
+
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import *
+from scipy.integrate import *
+
+
+############################
+
+#  Data Import from csv   
+
+############################
+
+
+#data in multiple csv files. Need to import and store in one dataframe/dictionary 
+
+
+
+
+
+
+
+
+
